69c91d7d5727e44c3deaaf4998d5c43eaaf21bcd
Yany Choi <dydghks0818@naver.com>
2023-11-04 12:16:38 +0900

    prepare deployment

--- db_info.py
+++ db_info.py
@@ -4,6 +4,6 @@ def db_info() :
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="0000",
-        database="sa"
+        password="1234",
+        database="sac"
     )
