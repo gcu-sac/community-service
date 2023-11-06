4700c8fe15e4be468dbc0a606155b560d801b4c3
Kim daehun <anonkorea@gachon.ac.kr>
2023-11-06 12:21:24 +0900

    method

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import db_info
 app = FastAPI()
 templates = Jinja2Templates(directory='./')
 
-
-
 import mysql.connector
 
 class SQL:
@@ -36,97 +34,106 @@ class SQL:
         self.db.close()
 
 sql = SQL()
-# @app.get("/community/article/{article_id}")
-# def upload_article(article_id: int, request: Request):
-#     name = request.query_params.get("name")
-#     title = request.query_params.get("title")
-#     content = request.query_params.get("content")
-#     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-#     try :
-#         sql.insert(f"INSERT INTO board (idx, name, title, content, date) VALUES ({article_id}, '{name}', '{title}', '{content}', '{current_time}')")
-#         return {"result" : "success"}
-#     except : 
-#         return {"result" : "fail"}
-
-# @app.get("/community/article/{article_id}")
-# def read_article(article_id: int):
-#     data = sql.select(f"SELECT * FROM board WHERE idx={article_id}")
-#     is_delete = sql.select(f"SELECT * FROM board WHERE idx={article_id} AND is_delete = 1")
-#     if data == [] :
-#         return {"result" : "none data"}
-#     elif is_delete != [] :
-#         return {"result" : "delete data"}
-#     else :
-#         return data[0]
-
-# @app.get("/community/article/{article_id}")
-# def update_article(article_id: int, request : Request):
-#     title = request.query_params.get("title")
-#     content = request.query_params.get("content")
-
-#     if title is None and content is None :
-#         return {"result" : "fail"}
-#     elif content is None : 
-#         sql.update(f"UPDATE board SET title={title} WHERE idx = {article_id}")
-#     elif title is None :
-#         sql.update(f"UPDATE board SET content={content} WHERE idx = {article_id}")
-#     else :
-#         sql.update(f"UPDATE board SET title = {title}, content={content} WHERE idx = {article_id}")
+@app.get("/community/article")
+def read_all_article():
+    data = sql.select(f"SELECT idx, name, title, content, date FROM board WHERE is_delete = 0")
     
-#     return {"result" : "success"}
+    return data
 
-# @app.get("/community/article/{article_id}")
-# def delete_article(article_id: int):
-#     data = sql.select(f"SELECT * FROM board WHERE idx={article_id}")
-#     is_delete = sql.select(f"SELECT * FROM board WHERE idx={article_id} AND is_delete = 1")
+@app.post("/community/article/{article_id}")
+async def upload_article(article_id: int, request: Request):
+    data = await request.json()
+    name = data.get("name")
+    title = data.get("title")
+    content = data.get("content")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-#     if data == [] :
-#         return {"result" : "none data"}
-#     elif is_delete != [] :
-#         return {"result" : "already delete"}
-#     else :
-#         sql.update(f"UPDATE board SET is_delete = 1 WHERE idx = {article_id}")
-#         return {"result" : "success"}
+    try :
+        sql.insert(f"INSERT INTO board (idx, name, title, content, date) VALUES ({article_id}, '{name}', '{title}', '{content}', '{current_time}')")
+        return {"result" : "success"}
+    except : 
+        return {"result" : "fail"}
 
-# @app.get("/community/article/{user_id}")
-# def read_user_article(user_id: str):
-#     try : 
-#         data = sql.select(f"SELECT title, content, date FROM board WHERE name={user_id} AND is_delete = 0")
-#         return data
-#     except : 
-#         return {"result" : "fail"}
+@app.get("/community/article/{article_id}")
+def read_article(article_id: int):
+    data = sql.select(f"SELECT * FROM board WHERE idx={article_id}")
+    is_delete = sql.select(f"SELECT * FROM board WHERE idx={article_id} AND is_delete = 1")
+    if data == [] :
+        return {"result" : "none data"}
+    elif is_delete != [] :
+        return {"result" : "delete data"}
+    else :
+        return data[0]
 
-# @app.get("/community/article/search/{keyword}")
-# def search_article(keyword: str):
-#     data = sql.select(f"SELECT name, title, content, date FROM board WHERE title LIKE '%{keyword}%' OR content LIKE '%{keyword}%'")
+@app.put("/community/article/{article_id}")
+def update_article(article_id: int, request: Request):
+    title = request.query_params.get("title")
+    content = request.query_params.get("content")
+
+    if title is None and content is None:
+        return {"result": "fail"}
+    elif content is None:
+        sql.update(f"UPDATE board SET title = '{title}' WHERE idx = {article_id}")
+    elif title is None:
+        sql.update(f"UPDATE board SET content = '{content}' WHERE idx = {article_id}")
+    else:
+        sql.update(f"UPDATE board SET title = '{title}', content = '{content}' WHERE idx = {article_id}")
+
+    return {"result": "success"}
+
+@app.delete("/community/article/{article_id}")
+def delete_article(article_id: int):
+    data = sql.select(f"SELECT * FROM board WHERE idx={article_id}")
+    is_delete = sql.select(f"SELECT * FROM board WHERE idx={article_id} AND is_delete = 1")
+
+    if data == [] :
+        return {"result" : "none data"}
+    elif is_delete != [] :
+        return {"result" : "already delete"}
+    else :
+        sql.update(f"UPDATE board SET is_delete = 1 WHERE idx = {article_id}")
+        return {"result" : "success"}
+
+@app.get("/community/article/user/{user_id}")
+def read_user_article(user_id: str):
+    try : 
+        data = sql.select(f"SELECT title, content, date FROM board WHERE name={user_id} AND is_delete = 0")
+        return data
+    except : 
+        return {"result" : "fail"}
+
+@app.get("/community/article/search/{keyword}")
+def search_article(keyword: str):
+    data = sql.select(f"SELECT name, title, content, date FROM board WHERE title LIKE '%{keyword}%' OR content LIKE '%{keyword}%'")
     
-#     if data != [] :
-#         return data
-#     else :
-#         return {"result" : "not found"}
+    if data != [] :
+        return data
+    else :
+        return {"result" : "not found"}
         
-# @app.get("/community/article/{article_id}/comment")
-# def reply(article_id: int, request: Request) :
-#     name = request.query_params.get("name")
-#     content = request.query_params.get("content")
-#     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+@app.get("/community/article/{article_id}/comment")
+async def reply(article_id: int, request: Request) :
+    data = await request.json()
+    name = data.get("name")
+    content = data.get("content")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
-#     if name is None or content is None :
-#         return {"result" : "fail"}
-#     else :
-#         sql.insert(f"INSERT INTO reply (bidx, name, content, date) VALUES({article_id}, '{name}', '{content}', '{current_time}')")
-#         data = sql.select(f"SELECT idx FROM reply WHERE date = '{current_time}'")
-#         return data[0]
+    if name is None or content is None :
+        return {"result" : "fail"}
+    else :
+        sql.insert(f"INSERT INTO reply (bidx, name, content, date) VALUES({article_id}, '{name}', '{content}', '{current_time}')")
+        data = sql.select(f"SELECT idx FROM reply WHERE date = '{current_time}'")
+        return data[0]
 
-# @app.get("/community/article/{article_id}/comment")
-# def read_reply(article_id: int) :
-#     data = sql.select(f"SELECT idx, name, content, date FROM reply WHERE bidx = '{article_id}'")
+@app.get("/community/article/{article_id}/comment")
+def read_reply(article_id: int) :
+    data = sql.select(f"SELECT idx, name, content, date FROM reply WHERE bidx = '{article_id}'")
 
-#     if data == [] :
-#         return {"result" : "fail"}
-#     else :
-#         return data
+    if data == [] :
+        return {"result" : "fail"}
+    else :
+        return data
 
 @app.get("/community/article/{article_id}/comment/{comment_id}")
 def delete_reply(article_id: int, comment_id: int) :
@@ -136,4 +143,4 @@ def delete_reply(article_id: int, comment_id: int) :
     if data == [] :
         return {"result" : "fail"}
     else :   
-        return data
+        return {"result" : "success"}
