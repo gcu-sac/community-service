2e7ed7ab00f4314053b82bf4c21e81d191fb2dfe
Yany Choi <dydghks0818@naver.com>
2023-11-07 13:09:55 +0900

    add env variables for db info

--- db_info.py
+++ db_info.py
@@ -1,9 +1,12 @@
 import mysql.connector
-
+from os import environ
 def db_info() :
+    host = environ.get('DB_HOST') or 'localhost'
+    user = environ.get('DB_USER') or 'root'
+    password = environ.get('DB_PASSWORD') or '1234'
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
+        host=host,
+        user=user,
+        password=password,
         database="sac"
     )
