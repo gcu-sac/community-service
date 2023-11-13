f2247348fb9c5bb7f22c6013064668c2165f200d
Yany Choi <dydghks0818@naver.com>
2023-11-13 18:17:31 +0900

    edit cookie to header

--- authenticate.py
+++ authenticate.py
@@ -6,7 +6,7 @@ def authenticate(token: str | None) -> bool:
     AUTH_URL = environ.get("AUTH_URL") or "https://sac.prod.cluster.yanychoi.com/api/auth"
     if token is None:
         raise HTTPException(status_code=401, detail="Unauthorized")
-    auth_request = requests.get(f"{AUTH_URL}/user/authenticate", cookies={"jwtAuthToken": token})
+    auth_request = requests.get(f"{AUTH_URL}/user/authenticate", headers={"jwt-auth-token": token})
     if auth_request.status_code == 200:
         return True
     raise HTTPException(status_code=401, detail="Unauthorized")
